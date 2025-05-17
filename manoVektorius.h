a3af4d6a0bfc4421eb2a1d68bb3caf58f75e8944
Liveta Ruskute <ruskutes@gmail.com>
2025-05-17 09:57:01 +0300

    mano vektoriui sukurtas konstruktorius ir destruktorius

--- manoVektorius.h
+++ manoVektorius.h
@@ -1,8 +1,31 @@
-#include <pagalbines.h>
 #ifndef FUNKCIJOS_H
 #define FUNKCIJOS_H
+#include <pagalbines.h>
+
+template <typename elemento_tipas>
+class Vektorius{
+
+private:
+    ElementoTipas* masyvas;
+    size_t dydis;
+    size_t talpa;
+public:
+    Vektorius();
+    ~Vektorius();
+};
 
 
+template <typename ElementoTipas>
+Vektorius<ElementoTipas>::Vektorius(){
+    talpa = 10;
+    dydis = 0;
+    masyvas = new ElementoTipas[talpa];
+}
+
+template <typename ElementoTipas>
+Vektorius<ElementoTipas>::~Vektorius() {
+    delete[] masyvas;
+}
 
 
 #endif
